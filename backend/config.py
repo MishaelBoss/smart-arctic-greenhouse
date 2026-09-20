f2a0cacda6ec74a20599ee4efb19f7f800f7e5f7
michael <michaelvoov@yandex.ru>
2026-09-20 11:56:05 +0500

    Add official submodules for app, backend, and arduino

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+psycopg://postgres:password@localhost:5432/greenhouse"
-)
